4bfc91020559092b73bd746492d9adc9413d487a
Riana ghd <ri.ghadamzadeh@gmail.com>
2025-11-05 18:07:38 +0100

    created class body

---
+++ User.cs
@@ -0,0 +1,35 @@
+using System.Reflection.Metadata;
+
+public class User
+{
+    public string Username;
+    public string Password;
+    public DateTime RegisterDate;
+    public string SearchHistory;
+
+    public void Register()
+    {
+
+    }
+
+    public void Login()
+    {
+
+    }
+
+    public void Logout()
+    {
+
+    }
+
+    public void DeleteAccount()
+    {
+
+    }
+    
+    public void GetHistory()
+    {
+        
+    }
+
+}
