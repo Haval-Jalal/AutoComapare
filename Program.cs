9f6ff7aa22bff4df2c9ac51d841d4818da46a012
Riana ghd <ri.ghadamzadeh@gmail.com>
2025-11-06 20:40:18 +0100

    worked on user class,

--- Program.cs
+++ Program.cs
@@ -1,10 +1,1 @@
-﻿namespace AutoCompare
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
