01439eb76687f23a53feab7f98910fcdcf0ae9a4
Pauline <plinekaeorg@gmail.com>
2025-11-10 13:52:49 +0100

    Added methods: AddItem(), RemoveItem() och FindItem(). And changed the structure of the lists

--- DataStore.cs
+++ DataStore.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AutoCompare
-{
+﻿
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -19,93 +12,59 @@ namespace AutoCompare
         //Klass
         public class DataStore<T>
         {
-            //privat attribut.
-            public List<T> User = new List<T>();
-            public List<T> Car = new List<T>();
-            //user lista
-            //car lista istället
+        //Attribut
+        public List<User> userStore = new List<User>();
+        public List<Car> carStore = new List<Car>();
+        
 
-            //Lägga till objekt
-            public void AddItem(T item)
+        //Lägga till objekt
+        public void AddItem(User user)
+        {
+            userStore.Add(user);
+            SaveToJson("users.json");
+        }
+
+        public bool RemoveItem(User user)
+        {
+            bool removed = userStore.Remove(user); 
+            if (removed)
+                SaveToJson("users.json");         
+            return removed;
+        }
+
+
+
+        public Car? FindItem(Predicate<Car> predicate)
+        {
+            return carStore.Find(predicate);
+        }
+
+       
+        public List<string>? FindItemHistory(string username)
+        {
+            User? user = userStore.Find(u => u.Username.Equals(username, StringComparison.OrdinalIgnoreCase));
+            return user?.SearchHistory;
+        }
+
+        
+        public User? FindUser(string username)
+        {
+            return userStore.Find(u => u.Username.Equals(username, StringComparison.OrdinalIgnoreCase));
+        }
+
+
+
+        //#Steg 2
+        public void SaveToJson(string filename)
             {
-                User.Add(item);
-                Car.Add(item);
-            }
-
-            //Ta bort objekt
-            public void RemoveItem(T item)
-            {
-                User.Remove(item);
-                Car.Remove(item);
-            }
-
-            //Alternativ #1 - bara user
-            //if (!Items.Remove(username))
-            //    Console.WriteLine($"The username {username.Name} was not found.");
-            //else
-            //    Console.WriteLine($"The username {username.Name} was removed.");
-
-
-            //Alternativ #2 - User och Car
-            //public void Remove(T item)
-            //{
-            //    if (!Items.Remove(item))
-            //    {
-            //        if (item is User u)
-            //            Console.WriteLine($"The username {u.Name} was not found.");
-            //        else if (item is Car c)
-            //            Console.WriteLine($"The car {c.Brand} {c.Model} was not found.");
-            //    }
-            //    else
-            //    {
-            //        if (item is User u)
-            //            Console.WriteLine($"The username {u.Name} was removed.");
-            //        else if (item is Car c)
-            //            Console.WriteLine($"The car {c.Brand} {c.Model} was removed.");
-            //    }
-            //}
-
-            //Alternativ #3 - User och Car -> ternary operator. Nytt inte lärt oss
-            //public void RemoveItem(T item)
-            //{
-            //    if (item is User u)
-            //        Console.WriteLine(!Items.Remove(item)
-            //            ? $"The username {u.Name} was not found."
-            //            : $"The username {u.Name} was removed.");
-            //    else if (item is Car c)
-            //        Console.WriteLine(!Items.Remove(item)
-            //            ? $"The car {c.Brand} {c.Model} was not found."
-            //            : $"The car {c.Brand} {c.Model} was removed.");
-            //}
-
-            //Alternativ #4 - item
-            //public void RemoveItem(T item)
-            //{
-            //    if (!Items.Remove(item))
-            //        Console.WriteLine("The item was not found.");
-            //    else
-            //        Console.WriteLine("The item was removed.");
-            //}
-
-
-            //Hitta objekt
-            public T FindItem(Predicate<T> predicate)
-            {
-                //returnerar ett resultat - ett objekt
-                return Items.Find(predicate);
-            }
-
-            //#Steg 2
-            public void SaveToJson(string filename)
-            {
-
+               
             }
 
             public void LoadFromJson(string filename)
             {
-
+               
             }
         }
     }
 
-}
+
