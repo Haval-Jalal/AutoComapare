ccd57e09a0c7ed68b2cdbcb8632b12998b945dd4
Haval-Jalal <Haval.jalal@hotmail.com>
2025-11-11 13:02:38 +0100

    .Add/.RemoveItem now work with any type. FindUser commented out - move to User class Added function to Save & Load Json

--- DataStore.cs
+++ DataStore.cs
@@ -12,11 +12,12 @@ namespace AutoCompare
     //Klass
     public class DataStore<T>
     {
-         //privat attribut.
-        public List<User> userStore = new List<User>();//get; set;???
-        public List<Car> carStore = new List<Car>();
-        public List<CarSearch> carSearchStore = new List<CarSearch>();
+        // //privat attribut.
+        //public List<User> userStore = new List<User>();//get; set;???
+        //public List<Car> carStore = new List<Car>();
+        //public List<CarSearch> carSearchStore = new List<CarSearch>();
        
+        public List<T> List { get; set; } = new List<T>();
 
 
         //Lägga till objekt
@@ -46,30 +47,36 @@ namespace AutoCompare
             return List.Find(predicate);
         }
 
-       
-        public List<string>? FindItemHistory(string username)
-        {
-            User? user = userStore.Find(u => u.Username.Equals(username, StringComparison.OrdinalIgnoreCase));
-            return user?.SearchHistory;
-        }
 
-       
-        public User? FindUser(string username)
-        {
-            return userStore.Find(u => u.Username.Equals(username, StringComparison.OrdinalIgnoreCase));
-        }
+        //public List<string>? FindItemHistory(string username)
+        //{
+        //    User? user = userStore.Find(u => u.Username.Equals(username, StringComparison.OrdinalIgnoreCase));
+        //    return user?.SearchHistory;
+        //}
+
+
+        //public User? FindUser(string username)
+        //{
+        //    return userStore.Find(u => u.Username.Equals(username, StringComparison.OrdinalIgnoreCase));
+        //}
 
 
 
         //#Steg 2
         public void SaveToJson(string filename)
         {
-
+            var json = System.Text.Json.JsonSerializer.Serialize(List,
+                new System.Text.Json.JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(filename, json);
         }
 
         public void LoadFromJson(string filename)
         {
-
+            if (File.Exists(filename))
+            {
+                var json = File.ReadAllText(filename);
+                List = System.Text.Json.JsonSerializer.Deserialize<List<T>>(json) ?? new List<T>();
+            }
         }
     }
 }
